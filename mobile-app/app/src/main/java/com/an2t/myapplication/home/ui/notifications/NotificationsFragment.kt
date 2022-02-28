3c3c7c84fe180480ac3048c7d219dba17fe2cc79
asa-404 <97547432+asa-404@users.noreply.github.com>
2022-02-27 17:05:29 -0800

    addded notification handler on android end

--- mobile-app/app/src/main/java/com/an2t/myapplication/home/ui/notifications/NotificationsFragment.kt
+++ mobile-app/app/src/main/java/com/an2t/myapplication/home/ui/notifications/NotificationsFragment.kt
@@ -1,16 +1,30 @@
 package com.an2t.myapplication.home.ui.notifications
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.an2t.myapplication.R
 import com.an2t.myapplication.databinding.FragmentNotificationsBinding
+import com.an2t.myapplication.home.HomeActivity
+
 //import com.an2t.myapplication.home1.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -23,6 +37,9 @@ class NotificationsFragment : Fragment() {
 
     private lateinit var notiAdapter: NotificationAdapter
 
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,10 +51,12 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textNotifications
-//        notificationsViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+
+        val btnSendNotification: Button = binding.btnSendNotification
+        btnSendNotification.setOnClickListener {
+            sendNotification()
+        }
+        createNotificationChannel()
         return root
     }
 
@@ -60,6 +79,54 @@ class NotificationsFragment : Fragment() {
         }
     }
 
+
+    fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager = activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(){
+
+
+        context?.let {
+            val intent = Intent(activity,HomeActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+
+            val pendingIntent = PendingIntent.getActivity(it,0,intent,0)
+
+            val bitmap = BitmapFactory.decodeResource(activity?.applicationContext?.resources,
+                R.drawable.login_image
+                )
+
+            val bitmapLargeIcon = BitmapFactory.decodeResource(activity?.applicationContext?.resources,
+                R.drawable.home_purple
+            )
+
+
+            val builder = NotificationCompat.Builder(it, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Example Title")
+                .setContentInfo("Example Description")
+                .setLargeIcon(bitmapLargeIcon)
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+            with(NotificationManagerCompat.from(it)){
+                notify(notificationId, builder.build())
+            }
+        }
+    }
 
 
     override fun onDestroyView() {
