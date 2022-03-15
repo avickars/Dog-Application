696f17d4917787ac9eddeefdd5614bdfb10f9521
asa-404 <97547432+asa-404@users.noreply.github.com>
2022-03-15 16:28:30 -0700

    Added the fcm token in the flow of app, backend and database. this will be used to send mobile notification to a specific user

--- mobile-app/app/src/main/java/com/an2t/myapplication/spash/SplashActivity.kt
+++ mobile-app/app/src/main/java/com/an2t/myapplication/spash/SplashActivity.kt
@@ -8,8 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.an2t.myapplication.R
 import com.an2t.myapplication.home.HomeActivity
 import com.an2t.myapplication.login.LoginActivity
+import com.an2t.myapplication.model.LoginRes
+import com.an2t.myapplication.utils.AppConstants.Companion.FCM_TOKEN
 import com.an2t.myapplication.utils.AppConstants.Companion.REFRESH_TOKEN
 import com.an2t.myapplication.utils.AppConstants.Companion.SHARED_PREF_DOG_APP
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
 
 
 class SplashActivity : AppCompatActivity() {
@@ -19,19 +23,33 @@ class SplashActivity : AppCompatActivity() {
         val sharedPreference =  getSharedPreferences(SHARED_PREF_DOG_APP, Context.MODE_PRIVATE)
         val refresh_token = sharedPreference.getString(REFRESH_TOKEN,"")
         val handler = Handler()
-        handler.postDelayed(
-            Runnable {
-                refresh_token?.let {
-                    if(it.isEmpty()){
-                        val i = Intent(this, LoginActivity::class.java)
-                        startActivity(i)
-                        finish()
-                    }else{
-                        val i = Intent(this, HomeActivity::class.java)
-                        startActivity(i)
-                        finish()
+
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(
+            this
+        ) { instanceIdResult: InstanceIdResult ->
+            val newToken = instanceIdResult.token
+            saveFCMToken(newToken)
+            handler.postDelayed(
+                Runnable {
+                    refresh_token?.let {
+                        if(it.isEmpty()){
+                            val i = Intent(this, LoginActivity::class.java)
+                            startActivity(i)
+                            finish()
+                        }else{
+                            val i = Intent(this, HomeActivity::class.java)
+                            startActivity(i)
+                            finish()
+                        }
                     }
-                }
-            }, 2000)
+                }, 2000)
+        }
+    }
+
+    private fun saveFCMToken(fcm_token: String) {
+        val editor = getSharedPreferences(SHARED_PREF_DOG_APP, MODE_PRIVATE).edit()
+        editor.putString(FCM_TOKEN, fcm_token)
+        editor.apply()
     }
 }
