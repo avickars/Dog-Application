3c14341c63e4c791655172b830bdbf3592c75c34
Karthiksrinatha <ksa166@sfu.ca>
2022-03-14 19:11:40 -0700

    Autostart for celery added with cron

---
+++ flask-celery-backend/autostart_celery.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+processId=$(ps -ef | grep 'celery -A app.celery worker --loglevel=INFO' | grep -v 'grep' | awk '{ printf $2 }')
+echo $processId
+if [ $processId ]
+then
+   echo "celery running"
+else
+   echo "celery not running"
+   cd /home/ubuntu/Dog-Application/flask-celery-backend
+   source env_name/bin/activate
+   celery -A app.celery worker --loglevel=INFO &
+   echo "celery started now"
+fi
