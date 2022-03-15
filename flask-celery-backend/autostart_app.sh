b74d21ce0b3efa3086825746581de22b8b80c699
Karthiksrinatha <ksa166@sfu.ca>
2022-03-14 19:07:13 -0700

    Autostart application script added

---
+++ flask-celery-backend/autostart_app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+processId=$(ps -ef | grep 'app.py' | grep -v 'grep' | awk '{ printf $2 }')
+echo $processId
+if [ $processId ]
+then
+   echo "Application running"
+else
+   echo "Application not running"
+   cd /home/ubuntu/Dog-Application/flask-celery-backend
+   source env_name/bin/activate
+   python app.py &
+   echo "Application started now"
+fi
